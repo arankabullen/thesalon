5c201f74e9b79ec085d5ec2c813bf156118a260d
Aranka Bullen <131798446+arankabullen@users.noreply.github.com>
2024-06-06 09:52:53 -0400

    established user attributes, created database config, updated package.json and added backend folder structures

---
+++ BACKEND/models/user.js
@@ -0,0 +1,31 @@
+/* 
+Author: Aranka Bullen
+*/
+
+const {DataTypes} = require('sequelize')
+const{ sequelize}= require('../config/database');
+
+const User = sequelize.define('User', {
+    userID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
