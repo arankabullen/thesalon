249ae6a1f3cc45f98acbf0ad496806ed707a0576
Aranka Bullen <131798446+arankabullen@users.noreply.github.com>
2024-06-10 02:50:17 -0400

    Update authcontroller.js

--- BACKEND/controllers/authcontroller.js
+++ BACKEND/controllers/authcontroller.js
@@ -1,61 +1,49 @@
-/**
+/***
  * Author: Aranka Bullen
- *
- * methods:
+ * 
+ * ref: https://www.npmjs.com/package/passport
  */
+ 
+import passport from "passport";
+import GoogleStrategy from "passport-google-oauth20";
+import User from "../models/user.js";
 
-const { User } = require("../models");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const passport = require('passport');
-
-const register = async (req, res) => {
-  const { name, email, password, phone } = req.body;
-  try {
-    const hashedPassword = await bcrypt(password, 10);
-    const user = await User.create({
-      name,
-      email,  
-      password: hashedPassword,
-      phone,
-    });
-    res.status(201).json({ message: "User registered successfully", user });
-  } catch (error) {
-    res.status(400).json({ message: "Error registering user", error });
-  }
-};
-
-const login = async (req, res) => {
-    const {email, password} = req.body;
-  try {
-    const user = await User.findOne({where: email});
-    if(!user){
-        res.status(404).json({message:"User not found"});
-    }
-    const isFound= await bcrypt.compare(password, user.password)
-    if(!isFound){
-        res.status(400).json({message:"Password incorrect"});
-    }
-    const token = jwt.sign({id: user.userID}, process.env.JWT_SECRET, {expiresIn: '2h'});
-
-    res.status(200).json({token, user});
-  } 
-  catch(error) {
-        res.status(400).json({message:" Error logging in", error});
-  }
-};
-
-const googleLogIn = passport.authenticate('google', {scope: ['profile', 'email']});
-
-const googleCallBack = (req, res) => {
-    passport.authenticate('google', {session: false}, (err, user, info) => {
-        if(err || !user){
-            res.status(400).json({message: "Unable to login in to Google account", err});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackUrl: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ where: { googleId: profile.id } });
+        if (!User) {
+          user = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+          });
         }
-        const token = jwt.sign({id:user.userID}, process.env.JWT_SECRET, {expiresIn: "2h"});
-        res.status(201).json({token, user});
-    })
-    (req, res);
-}
+        return done(null, user);
+      } catch (error) {
+        return done(error, false);
+      }
+    }
+  )
+);
 
-module.exports = {register, login, googleLogIn, googleCallBack};
+passport.serializeUser((user, done) => {
+  done(null, user.userid);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findByPk(id);
+    done(null, user);
+  } catch (error) {
+    return done(error, null);
+  }
+});
+
+export default passport;
